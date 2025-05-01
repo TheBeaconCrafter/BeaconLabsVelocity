222af3d01db4d7186c1248f54c6513cbd70daa1b
Vincent Wackler <vincent@bcnlab.org>
2025-05-01 11:59:09 +0200

    Initial commit

--- src/main/java/org/bcnlab/beaconLabsVelocity/BeaconLabsVelocity.java
+++ src/main/java/org/bcnlab/beaconLabsVelocity/BeaconLabsVelocity.java
@@ -1,18 +1,78 @@
 package org.bcnlab.beaconLabsVelocity;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bcnlab.beaconLabsVelocity.command.LabsVelocityCommand;
 import org.slf4j.Logger;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.loader.ConfigurationLoader;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 @Plugin(id = "beaconlabsvelocity", name = "BeaconLabsVelocity", version = "1.0.0", url = "bcnlab.org", authors = {"Vincent Wackler"})
 public class BeaconLabsVelocity {
 
     @Inject
+    @DataDirectory
+    private Path dataDirectory;
+
+    private ConfigurationNode config;
+
+    private String prefix;
+    private final String version = "1.0.0";
+
+    @Inject
     private Logger logger;
+
+    @Inject
+    public BeaconLabsVelocity(CommandManager commandManager) {
+        commandManager.register("labsvelocity", new LabsVelocityCommand(this));
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        Path configFile = dataDirectory.resolve("config.yml");
+
+        try {
+            if (!Files.exists(configFile)) {
+                Files.createDirectories(dataDirectory);
+                Files.copy(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("config.yml")), configFile);
+            }
+
+            ConfigurationLoader<?> loader = YamlConfigurationLoader.builder()
+                    .path(configFile)
+                    .build();
+
+            config = loader.load();
+            prefix = config.node("prefix").getString("&6BeaconLabs &8» ");
+
+        } catch (IOException e) {
+            logger.error("Failed to load config!", e);
+            prefix = "&4ConfigError &8» ";
+        }
+
+        logger.info("BeaconLabsVelocity is initialized!");
+    }
+
+    public Component getPrefix() {
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(prefix);
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public ConfigurationNode getConfig() {
+        return config;
     }
 }
